25a35c1033fc4dc6d7332f22349a026ad8b439f0
christinesparks <christine.sparks@ni.com>
2019-03-04 11:36:33 -0800

    Update BuildConfiguration.groovy

--- src/ni/vsbuild/BuildConfiguration.groovy
+++ src/ni/vsbuild/BuildConfiguration.groovy
@@ -16,17 +16,16 @@ class BuildConfiguration implements Serializable {
    public final def build
    public final def dependencies
    public final def packageInfo
-      
    public final def test
 
-   private BuildConfiguration(archive, projects, codegen, build, dependencies, packageInfo, test) {
+   private BuildConfiguration(archive, projects, codegen, build, dependencies, packageInfo) {
       this.archive = archive
       this.projects = projects
       this.codegen = codegen
       this.build = build
       this.dependencies = dependencies
       this.packageInfo = packageInfo
-      this.test = test
+      //this.test = test
    }
 
    static BuildConfiguration load(def script, String jsonFile) {      
@@ -47,7 +46,7 @@ class BuildConfiguration implements Serializable {
          convertedJson.build,
          convertedJson.dependencies,
          convertedJson.package,
-         convertedJson.test)
+         //convertedJson.test)
    }
 
    public void printInformation(script) {
@@ -59,7 +58,7 @@ class BuildConfiguration implements Serializable {
             Build: $build
             Dependencies: $dependencies
             Package: $packageInfo
-            Test: $test
+            Test:
          """.stripIndent()
 
       script.echo configurationString
