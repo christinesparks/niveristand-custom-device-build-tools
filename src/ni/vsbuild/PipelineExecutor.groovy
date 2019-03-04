840103dc7f4abe3b1971ff9b25497d38a3f49d03
christinesparks <christine.sparks@ni.com>
2019-03-04 10:40:44 -0800

    Update PipelineExecutor.groovy

--- src/ni/vsbuild/PipelineExecutor.groovy
+++ src/ni/vsbuild/PipelineExecutor.groovy
@@ -3,9 +3,15 @@ package ni.vsbuild
 class PipelineExecutor implements Serializable {
 
    static void execute(script, String nodeLabel, List<String> lvVersions, List<String> dependencies = []) {
+      
+      script.echo "Starting Pipeline"
+      
       def pipelineInformation = new PipelineInformation(nodeLabel, lvVersions, dependencies)
       pipelineInformation.printInformation(script)
       def pipeline = new Pipeline(script, pipelineInformation)
+      
+      script.echo "Executing Pipeline"
+      
       pipeline.execute()
    }
    
