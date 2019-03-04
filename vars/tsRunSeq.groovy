dd29b4c04fc9b5e7fdf7640c111ab27d6c693aeb
christinesparks <christine.sparks@ni.com>
2019-03-04 13:38:52 -0800

    Update tsRunSeq.groovy

--- vars/tsRunSeq.groovy
+++ vars/tsRunSeq.groovy
@@ -1,1 +1,5 @@
-
+def call(seq, tsVersion){
+   echo "Running $seq."
+   //def logFileName = getLogName(vi)
+   //labviewcli("-OperationName SecureRunVI -VIPath \"$WORKSPACE\\$vi\" -LogFilePath \"$WORKSPACE\\lvRunVi_${logFileName}.log\"", lvVersion)
+}
