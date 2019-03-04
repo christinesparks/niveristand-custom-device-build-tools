b3bd0982b77ac6179d2f888b8d6fa76360ac12cf
christinesparks <christine.sparks@ni.com>
2019-03-04 12:10:28 -0800

    Update Test.groovy

--- src/ni/vsbuild/stages/Test.groovy
+++ src/ni/vsbuild/stages/Test.groovy
@@ -4,13 +4,9 @@ class Test extends AbstractStepStage {
 
    Test(script, configuration, lvVersion) {
       super(script, 'Test', configuration, lvVersion)
-      script.echo 'New Test'
    }
 
    void executeStage() {
-      
-      script.echo '$configuration.test'
-      
       executeSteps(configuration.test)
    }
 }
