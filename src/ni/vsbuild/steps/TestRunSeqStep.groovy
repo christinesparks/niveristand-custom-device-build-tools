811ba860b60b694802d9e08d38e5011c4945dc29
christinesparks <christine.sparks@ni.com>
2019-03-05 08:12:35 -0800

    Update TestRunSeqStep.groovy

--- src/ni/vsbuild/steps/TestRunSeqStep.groovy
+++ src/ni/vsbuild/steps/TestRunSeqStep.groovy
@@ -5,18 +5,17 @@ import ni.vsbuild.BuildConfiguration
 class TestRunSeqStep extends TestStep {
 
    def seq
+   def tsVersion
+   def tsBitness
 
    TestRunSeqStep(script, mapStep) {
       super(script, mapStep)
       this.seq = mapStep.get('seq')
+      this.tsVersion = mapStep.get('teststand_version')
+      this.tsBitness = mapStep.get('teststand_bitness')
    }
 
    void executeStep(BuildConfiguration configuration) {
-      
-      def debugString = self.seq
-      
-      script.echo 'Called Execute Test Step with sequence $debugString'
-      
-      //script.tsRunSeq(seq, tsVersion)
+      script.tsRunSeq(seq, tsVersion, tsBitness)
    }
 }
