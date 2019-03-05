5fb389c583f76e4a627edf8fa7a4764c3ed2b7a3
christinesparks <christine.sparks@ni.com>
2019-03-04 17:11:36 -0800

    Update TestStep.groovy

--- src/ni/vsbuild/steps/TestStep.groovy
+++ src/ni/vsbuild/steps/TestStep.groovy
@@ -7,5 +7,6 @@ abstract class TestStep extends AbstractStep {
    TestStep(script, mapStep) {
       super(script, mapStep)
       //this.tsVersion = tsVersion
+      script.echo 'this is the generic test step'
    }
 }
