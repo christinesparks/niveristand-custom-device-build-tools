6446d512d9a141fb0d026041648e7e406589e7dd
christinesparks <christine.sparks@ni.com>
2019-03-05 08:08:36 -0800

    Update tsRunSeq.groovy

--- vars/tsRunSeq.groovy
+++ vars/tsRunSeq.groovy
@@ -1,5 +1,21 @@
-def call(seq, tsVersion){
-   echo "Running $seq."
-   //def logFileName = getLogName(vi)
-   //labviewcli("-OperationName SecureRunVI -VIPath \"$WORKSPACE\\$vi\" -LogFilePath \"$WORKSPACE\\lvRunVi_${logFileName}.log\"", lvVersion)
+def call(seq, tsVersion, tsBitness){
+   echo "Running test $seq with TestStand $tsVersion."
+   
+   def programFiles = "C:\\Program Files"
+
+   if (tsBitness == '32') {
+      programFiles = programFiles+' (x86)'
+   } 
+
+   def seqEditorPath = programFiles+"\\National Instruments\\TestStand ${tsVersion}\\Bin\\SeqEdit.exe"
+
+   def tsVersionSelectorPath = "C:\\Program Files (x86)\\National Instruments\\Shared\\TestStand Version Selector\\TSVerSelect.exe"
+   def sequencePath = "${WORKSPACE}\\${seq}"
+
+   formattedTSVersion = tsVersion.substring(2,4)+".0"
+
+   bat "\"${tsVersionSelectorPath}\" /version ${formattedTSVersion} /installing /noprompt"
+   bat "\"${seqEditorPath}\" /outputToStdIO /run MainSequence \"${sequencePath}\" /quit"
+
+}
 }
