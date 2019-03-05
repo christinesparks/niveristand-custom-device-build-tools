49e9bc867d9e71f2e6d8d5073debb4dc1788284e
christinesparks <christine.sparks@ni.com>
2019-03-04 16:04:08 -0800

    Update Test.groovy

--- src/ni/vsbuild/stages/Test.groovy
+++ src/ni/vsbuild/stages/Test.groovy
@@ -8,7 +8,7 @@ class Test extends AbstractStepStage {
 
    void executeStage() {
       script.echo 'I am $configuration.test.sequence_location'
-      script.echo 'My name is $this.name
+      script.echo 'My name is $this.name'
       executeSteps(configuration.test)
    }
 }
